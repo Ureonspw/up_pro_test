6cf87fd35bf073f52043eac8202ef6fb7c457f0b
Ureonspw <Ureon206@gmail.com>
2025-05-26 06:21:30 +0400

    import_page_commit

--- resources/js/Pages/importation_cours/importation_quizz.tsx
+++ resources/js/Pages/importation_cours/importation_quizz.tsx
@@ -1,10 +1,29 @@
-import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout';
+import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout";
+import { FaLink } from "react-icons/fa6";
 
+import quizzcss from "../../../css/importation_cours/importation_quizz.module.css";
 export default function ImportationQuizz() {
     return (
         <AuthenticatedLayout>
-            <div>
-                <h1>Importation Quizz</h1>
+            <div className={quizzcss.containerbox}>
+                <div className={quizzcss.title}>Importation de cours</div>
+
+                <div className={quizzcss.containerimportpdf}>
+                    {" "}
+                    <h1>Glissez ici le fichier que vous voulez importer ...</h1>
+
+                    <form action="/import_doc">
+
+                        <label htmlFor="importf"> non du fichier <FaLink /></label>
+                        <input
+                            id="importf"
+                            type="file"
+                            className={quizzcss.importpdf}
+                        />
+                    </form>{" "}
+                    <h2>Taille max : 100MB</h2>
+
+                </div>
             </div>
         </AuthenticatedLayout>
     );
